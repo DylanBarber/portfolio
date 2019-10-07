31b3c37807950084a3b0efa92b9b3012f5741e1e
Dylan Barber <DylanBarber1@Outlook.com>
2019-10-07 15:37:31 -0400

    Add picture and text on About Me page

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,8 +1,18 @@
 //Depenedencies
-import React from 'react'
+import React from "react"
 
 const AboutMe = () => (
-  <h1>About Me Test</h1>
+  <div className="aboutMeContainer">
+    <div className="aboutMeDiv">
+      <img alt="Dylan Barber - Full Stack Developer" className="aboutMePicture" src="./lib/img/dylanBarberPic.png" /> 
+      <p className="aboutMeText">I have 4 months professional experience in Javascript, Node JS, React JS, test driven development using Chai and Mocha JS, and many more.
+      Below you can download my resume which outlines my 3 years experience in the IT field ranging from the beginning at help desk, to network
+      engineering, and ending up currently in software engineering. I have been almost entirely self taught through my career and have an optimistic progressive
+      attitude at self development that helps me learn new technology concepts everyday to better my work. I currently work at Road to Hire in Detroit, MI where
+      I utilize my knowledge in JS and various frameworks to create web apps and API's to support their data.
+      </p>
+    </div>
+  </div>
 )
 
 export default AboutMe; 
