7d0b25faaad93b314bb8965212b444af4fecd502
Dylan Barber <DylanBarber1@Outlook.com>
2019-10-08 16:29:54 -0400

    Fix title bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 //Components
-import Header from './components/Header';
-import AboutMe from './components/AboutMe';
-import Projects from './components/Projects';
-import Experience from './components/Experience';
-import Footer from './components/Footer'
+import Header from "./components/Header";
+import AboutMe from "./components/AboutMe";
+import Projects from "./components/Projects";
+import Experience from "./components/Experience";
+import Footer from "./components/Footer";
 
 function App() {
   return (
