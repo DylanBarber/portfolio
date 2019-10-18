e1c952cfe94e2711d7659fec0404804929d73a2c
Dylan Barber <DylanBarber1@Outlook.com>
2019-10-18 10:37:32 -0400

    Add more to about me and change footer date

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -9,7 +9,7 @@ const AboutMe = () => (
     <div className="content">
       <img alt="Dylan Barber - Full Stack Developer" className="aboutMePicture" src="./lib/img/dylanBarberPic.png" /> 
       <h1>Dylan Barber</h1>
-      <p className="aboutMeText">I am a Full Stack Software Developer in Detroit, Michigan. I have experience in Javascript, Node JS, React JS, Electron JS, test driven development using Chai and Mocha JS, and many more technologies.
+      <p className="aboutMeText">I am a Full Stack Software Developer in Detroit, Michigan. I have experience in Javascript, Node JS, React JS, Electron JS, test driven development using Chai and Mocha JS, CI/CD, MongoDB and many other technologies.
       Below you can download my resume which outlines my 3 years experience in the IT field ranging from the beginning at help desk, to network
       engineering, and ending up currently in software engineering. I have been almost entirely self taught through my career and have an optimistic progressive
       attitude at self development that helps me learn new technology concepts everyday to better my work. I currently work at Road to Hire in Detroit, MI where
